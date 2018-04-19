cc041cf0c5217ceff1dfe149e10f63a49bc661b3
yohanessahrul <yohanessahrul92@gmail.com>
2018-04-19 22:09:30 +0700

    resolve conflict vue

--- src/router.js
+++ src/router.js
@@ -6,20 +6,11 @@ import Game from './views/Game.vue'
 import Room from './views/Room.vue'
 import Index from './views/Index.vue'
 import Board from './views/Board.vue'
-import Home from './views/Home.vue'
-import Loby from './views/Loby.vue'
-import Game from './views/Game.vue'
-import Room from './views/Room.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
     {
       path: '/',
       name: 'index',
